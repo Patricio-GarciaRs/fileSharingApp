0c94c00d6e912ed557c92933f135861e5470cdb9
Indie Beer UPN <patricio.gar14@gmail.com>
2024-09-02 18:54:46 -0600

    endpoint for testing

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -4,6 +4,16 @@ import upload from '../utils/upload.js';
 
 const router = express.Router();
 
+router.get('/hello', (req, res)=>{
+
+    const messageString = {
+        message: 'This is a test message'
+    }
+
+    res.json(messageString);
+
+})
+
 router.post('/upload', upload.single('file'), uploadImage)
 
 router.get('/file/:fileId', downloadImage);
